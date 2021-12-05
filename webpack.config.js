f88d318db30574e6c977d4b4f93232f02c630ce2
tkn <tkn_dev@hotmail.com>
2021-12-05 13:19:43 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./src/index.jsx",
+  output: {
+    path: path.join(__dirname, "dist", "assets"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node-modules/,
+        loader: "babel-loader",
+        options: {
+          plugins: ["@babel/plugin-transform-runtime"],
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node-modules/,
+        use: ["style-loader", "css-loader"],
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx", ".json"]
+  },
+  devtool: "source-map"
+};
