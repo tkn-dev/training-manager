4f70ebc357fcaf029d5443e3cc28e7564b543332
tkn <tkn_dev@hotmail.com>
2021-12-15 19:06:43 +0900

    herokuデプロイテスト

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
 /* eslint-disable no-console */
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, '../dist')));
+
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello!' });
 });
 
 app.listen(port, () => {
--- src/features/CreateExercise.jsx
+++ src/features/CreateExercise.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function CreateExercise(onCreate = (f) => f) {
+  const [message, setMessage] = useState();
+  useEffect(() => {
+    fetch('http://localhost:3000/api')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  });
   return (
-    <>
-      <h1>種目登録</h1>
-      <p>名前</p>
-      <input id="createExercise" type="text" />
-      <button
-        style={{ width: 60, height: 20 }}
-        onClick={() => onCreate(document.getElementById('searchUser').value)}
-        type="submit"
-      >
-        登録
-      </button>
-    </>
+    <div className="App">
+      <h1>front end</h1>
+      <p>{message}</p>
+    </div>
+    // <>
+    //   <h1>種目登録</h1>
+    //   <p>名前</p>
+    //   <input id="createExercise" type="text" />
+    //   <button
+    //     style={{ width: 60, height: 20 }}
+    //     onClick={() => onCreate(document.getElementById('searchUser').value)}
+    //     type="submit"
+    //   >
+    //     登録
+    //   </button>
+    // </>
   );
 }
